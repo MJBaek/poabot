c5bff28e1f1a63b09ad6c79488d6defa1db045b8
benjamin <benjamin@lunamint.com>
2019-06-11 11:31:25 +0900

    upd

--- gaiacli/gaiaclient.js
+++ gaiacli/gaiaclient.js
@@ -67,7 +67,7 @@ module.exports = function (logger) {
 						}
 						jsonRes.code = 200
 						jsonRes.msg = 'success'
-						jsonRes.amount = parseFloat(amount)
+						jsonRes.amount = Math.floor(amount)
 						jsonRes.denom = 'uatom'
 						jsonRes = JSON.stringify(jsonRes)
 						resolve(jsonRes)
